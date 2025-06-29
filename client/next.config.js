71a59309d46863b151f594d9049ec451a62742dd
Mike Fretwell <mfretwell@mGeo.ca>
2025-06-29 00:10:33 -0600

    Stage 2.2: dbase connection success

--- client/next.config.js
+++ client/next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
         source: '/uploads/:path*',
         destination: 'http://localhost:5000/uploads/:path*',
       },
+      {
+        source: '/api/auth/:path*',
+        destination: 'http://localhost:5000/api/auth/:path*',
+      },
     ];
   },
   images: {
